58e61efe52e4f3779e701feb118dbd5b7e113457
Sydney John <sydney22john@gmail.com>
2024-04-25 15:59:20 -0600

    features and design pseudo laid out

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
+"""
+Goals of this application.
+- Ability to step through maze generation algorithm
+- auto play maze generation
+- specify the size of the maze (with restrictions)
+- disable/enable maze generation visualization
+- specify the algorithm
+- color legend (in a separate help tab or legend tab??)
+- reset, play, stop buttons
+- animation speed
+
+Other features:
+- specify the color scheme
+- solve the maze option
+"""
+
+"""
+Design:
+
+-----------------------------------------
+| <algo> | <size> | <animate> | <h> |   |
+-----------------------------------------
+|                                   | l |
+|                                   | e |
+|                                   | g |
+|         <maze generation>         | e |
+|                                   | n |
+|                                   | d |
+|                                   | ? |
+-----------------------------------------
+| <start> | <stop> | <reset> | <export> |
+-----------------------------------------
+| <speed> |                             |
+-----------------------------------------
+
+"""
+
 if __name__ == "__main__":
     print("hello world")
